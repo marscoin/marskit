1c6655e6178e91bb5da2c65d4c5c7c639175c602
Corey Phillips <corey.lyle.phillips@protonmail.com>
2020-12-07 15:03:45 -0500

    Created wallet shapes in shapes/wallet.ts. Added wallet types to types/wallet.ts.

--- src/store/types/wallet.ts
+++ src/store/types/wallet.ts
@@ -5,6 +5,53 @@ export interface IWallet {
     error: boolean;
     selectedNetwork: AvailableNetworks;
     selectedWallet: string;
-    wallets: object;
+    wallets: { [key: string]: IDefaultWalletShape } | {};
     [key: string]: any;
+}
+
+export type AddressType = "bech32" | "segwit" | "legacy";
+
+export interface IWalletItem<T> {
+    bitcoin: T,
+    bitcoinTestnet: T,
+    timestamp?: number | null
+}
+
+export interface IAddress {
+    [key: string]: {
+        path: string,
+        address: string,
+        scriptHash: string
+    } | []
+}
+
+export interface IAddresses {
+    bitcoin: IAddress[] | [],
+    bitcoinTestnet: IAddress[] | [],
+    timestamp: null
+}
+
+export interface IDefaultWalletShape {
+    id: string,
+    name: string,
+    type: string,
+    addresses: IAddresses[] | IWalletItem<[]>,
+    addressIndex: IWalletItem<number>,
+    changeAddresses: IAddresses[] | IWalletItem<[]>,
+    changeAddressIndex: IWalletItem<number>,
+    utxos: IWalletItem<[]>,
+    transactions: IWalletItem<[]>,
+    blacklistedUtxos: IWalletItem<[]>,
+    confirmedBalance: IWalletItem<number>,
+    unconfirmedBalance: IWalletItem<number>,
+    lastUpdated: IWalletItem<number>,
+    hasBackedUpWallet: boolean,
+    walletBackupTimestamp: string,
+    keyDerivationPath: IWalletItem<string>,
+    networkTypePath: IWalletItem<string>,
+    addressType: {
+        bitcoin: AddressType,
+        bitcoinTestnet: AddressType
+    },
+    rbfData: IWalletItem<object>
 }
