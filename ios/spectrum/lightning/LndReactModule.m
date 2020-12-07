7ebd9baf875e41520af4ccef85f762a706ebe70a
Jason van den Berg <jayvdb1@gmail.com>
2020-12-07 13:05:25 +0200

    Linked iOS native files

---
+++ ios/spectrum/lightning/LndReactModule.m
@@ -0,0 +1,62 @@
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+//MARK: LND functions
+@interface RCT_EXTERN_MODULE(LndReactModule, NSObject)
+
+RCT_EXTERN_METHOD(
+                  walletExists: (NSString *)network
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  currentState: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  start: (NSString *)configContent
+                  network: (NSString *)network
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  genSeed: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  createWallet: (NSString *)password
+                  seed: (NSArray *)seed
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  unlockWallet: (NSString *)password
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  sendCommand: (NSString *)method
+                  body: (NSString *)seed
+                  resolve: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+
+RCT_EXTERN_METHOD(
+                  sendStreamCommand: (NSString *)method
+                  streamId: (NSString *)streamId
+                  body: (NSString *)body
+                  )
+@end
+
+//MARK: Events
+@interface RCT_EXTERN_MODULE(LightningEventEmitter, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(supportedEvents)
+
+@end
