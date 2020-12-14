fbad40c8ce09e0ec54bd8cd128833de2770ff0b3
Corey Phillips <corey.lyle.phillips@protonmail.com>
2020-12-14 16:53:31 -0500

    Added CREATE_WALLET to actions.ts. Added createWallet method to actions/wallet.ts. Added CREATE_WALLET condition to reducers/wallet.ts.

--- src/store/actions/wallet.ts
+++ src/store/actions/wallet.ts
@@ -1,4 +1,17 @@
 import actions from "./actions";
+import { ICreateWallet } from "../types/wallet";
+import {
+	generateAddresses,
+	generateMnemonic,
+	getMnemonicPhrase,
+	validateMnemonic
+} from "../../utils/wallet";
+import { getDispatch, getStore } from "../helpers";
+import { setKeychainValue } from "../../utils/helpers";
+import { availableNetworks } from "../../utils/networks";
+import { defaultWalletShape } from "../shapes/wallet";
+
+const dispatch = getDispatch();
 
 export const updateWallet = payload => dispatch => {
 	return new Promise(async resolve => {
@@ -7,5 +20,65 @@ export const updateWallet = payload => dispatch => {
 			payload
 		});
 		resolve({ error: false, data: "" });
+	});
+};
+
+export const createWallet = (
+	{
+		wallet = "wallet0",
+		addressAmount = 2,
+		changeAddressAmount = 2,
+		mnemonic = "",
+		keyDerivationPath = "84"
+	}: ICreateWallet) => {
+	return new Promise(async (resolve) => {
+		const failure = (data) => resolve({ error: true, data });
+		try {
+			const getMnemonicPhraseResponse = await getMnemonicPhrase(wallet);
+			const { error, data } = getMnemonicPhraseResponse;
+			const { wallets } = getStore().wallet;
+			if (!error && data && wallet in wallets) return failure(`Wallet ID, "${wallet}" already exists.`);
+
+			//Generate Mnemonic if none was provided
+			if (mnemonic === "") {
+				mnemonic = validateMnemonic(data) ? data : await generateMnemonic();
+			}
+			//if (!validateMnemonic(mnemonic)) return failure("Invalid Mnemonic");
+			await setKeychainValue({ key: wallet, value: mnemonic });
+
+			//Generate a set of addresses & changeAddresses for each network.
+			const _addresses = {};
+			const _changeAddresses = {};
+			const networks = availableNetworks();
+			await Promise.all(
+				networks.map(async (network) => {
+					const generatedAddresses = await generateAddresses({
+						wallet,
+						selectedNetwork: network,
+						addressAmount,
+						changeAddressAmount,
+						keyDerivationPath
+					});
+					if (generatedAddresses.error) return failure(generatedAddresses.data);
+					const { addresses, changeAddresses } = generatedAddresses.data;
+					_addresses[network] = addresses;
+					_changeAddresses[network] = changeAddresses;
+				})
+			)
+			const payload = {
+				[wallet]: {
+					...defaultWalletShape,
+					addresses: _addresses,
+					changeAddresses: _changeAddresses
+				}
+			};
+
+			await dispatch({
+				type: actions.CREATE_WALLET,
+				payload
+			});
+
+			resolve({ error: false, data: "" });
+		} catch (e) {failure(e);}
 	});
 };
