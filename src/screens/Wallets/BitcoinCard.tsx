1c770ec81f2121dd227917bfa060b39bfbeaf050
Corey Phillips <corey.lyle.phillips@protonmail.com>
2020-12-15 00:34:42 -0500

    Created BitcoinCard.tsx & Receive.tsx. Implemented Receive in BitcoinCard.tsx.

---
+++ src/screens/Wallets/BitcoinCard.tsx
@@ -0,0 +1,62 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React, { memo, useState } from "react";
+import {
+	LayoutAnimation,
+	StyleSheet
+} from "react-native";
+import { View } from "../../styles/components";
+import Receive from "./Receive";
+import Button from "../../components/Button";
+import AssetCard from "../../components/AssetCard";
+
+const BitcoinCard = () => {
+	const [displayReceive, setDisplayReceive] = useState(false);
+	LayoutAnimation.easeInEaseOut();
+
+	return (
+		<AssetCard
+			title="Bitcoin Wallet"
+			assetBalanceLabel="0 BTC"
+			fiatBalanceLabel="$0"
+			assetType="bitcoin"
+		>
+			<>
+				<View color="transparent" style={styles.buttonRow}>
+					<Button
+						color="onSurface"
+						style={styles.sendButton}
+						text="Send"
+					/>
+					<Button
+						color="onSurface"
+						style={styles.receiveButton}
+						onPress={() => setDisplayReceive(!displayReceive)}
+						text="Receive"
+					/>
+				</View>
+				{displayReceive && <Receive />}
+			</>
+		</AssetCard>
+	);
+};
+
+const styles = StyleSheet.create({
+	buttonRow: {
+		flexDirection: "row",
+		marginTop: 10
+	},
+	sendButton: {
+		flex: 1,
+		marginRight: 5
+	},
+	receiveButton: {
+		flex: 1,
+		marginLeft: 5
+	}
+});
+
+export default memo(BitcoinCard);
---
+++ src/screens/Wallets/Receive.tsx
@@ -0,0 +1,70 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React, { memo, useEffect, useState } from "react";
+import {
+	LayoutAnimation,
+	StyleSheet,
+} from "react-native";
+import { View, Text } from "../../styles/components";
+import QRCode from "react-native-qrcode-svg";
+import Animated, { Easing } from "react-native-reanimated";
+import { useSelector } from "react-redux";
+import Store from "../../store/types";
+
+const updateOpacity = ({ opacity = new Animated.Value(0), toValue = 0, duration = 1000 }) => {
+	try {
+		Animated.timing(
+			opacity,
+			{
+				toValue,
+				duration,
+				easing: Easing.inOut(Easing.ease)
+			}
+		).start();
+	} catch {}
+};
+
+const Receive = () => {
+	const wallet = useSelector((state: Store) => state.wallet);
+	const [opacity] = useState(new Animated.Value(0));
+	const { selectedWallet, selectedNetwork } = wallet;
+	const key = Object.keys(wallet.wallets[selectedWallet].addresses[selectedNetwork])[0];
+	const address = wallet.wallets[selectedWallet].addresses[selectedNetwork][key].address;
+
+	useEffect(() => {
+		setTimeout(() => {
+			updateOpacity({ opacity, toValue: 1 });
+		}, 100);
+		return () => updateOpacity({ opacity, toValue: 0, duration: 0 });
+	}, []);
+
+	LayoutAnimation.easeInEaseOut();
+
+	return (
+		<Animated.View style={[styles.container, { opacity }]}>
+			<View color="transparent" style={{ alignItems: "center" }}>
+				<QRCode value={address} size={200} />
+				<Text style={styles.text}>
+					{address}
+				</Text>
+			</View>
+		</Animated.View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		marginVertical: 20
+	},
+	text: {
+		fontWeight: "bold",
+		fontSize: 14,
+		textAlign: "center",
+		marginTop: 10
+	}
+});
+
+export default memo(Receive);
