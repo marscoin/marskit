3cf649925474de1ab262d29bbca118a75dea14da
Jason van den Berg <jayvdb1@gmail.com>
2020-12-30 13:52:48 +0200

    Set notification position. Useful when for when iOS shows their clipboard warning over everything.

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,50 +1,52 @@
-import Toast from 'react-native-toast-message';
+import Toast, { ToastPosition } from 'react-native-toast-message';
 
 type AppNotification = {
 	title?: string;
 	message: string;
 };
 
-const topOffset = 40;
+const defaultOptions = {
+	autoHide: true,
+	visibilityTime: 4000,
+	topOffset: 40,
+	bottomOffset: 120,
+};
 
-export const showErrorNotification = ({
-	title = 'Something went wrong',
-	message,
-}: AppNotification): void => {
+export const showErrorNotification = (
+	{ title = 'Something went wrong', message }: AppNotification,
+	position: ToastPosition = 'top',
+): void => {
 	Toast.show({
 		type: 'error',
 		text1: title,
 		text2: message,
-		visibilityTime: 4000,
-		autoHide: true,
-		topOffset,
+		...defaultOptions,
+		position,
 	});
 };
 
-export const showSuccessNotification = ({
-	title = 'Success!',
-	message,
-}: AppNotification): void => {
+export const showSuccessNotification = (
+	{ title = 'Success!', message }: AppNotification,
+	position: ToastPosition = 'top',
+): void => {
 	Toast.show({
 		type: 'success',
 		text1: title,
 		text2: message,
-		visibilityTime: 4000,
-		autoHide: true,
-		topOffset,
+		...defaultOptions,
+		position,
 	});
 };
 
-export const showInfoNotification = ({
-	title = '',
-	message,
-}: AppNotification): void => {
+export const showInfoNotification = (
+	{ title = '', message }: AppNotification,
+	position: ToastPosition = 'top',
+): void => {
 	Toast.show({
 		type: 'info',
 		text1: title,
 		text2: message,
-		visibilityTime: 6000,
-		autoHide: true,
-		topOffset,
+		...defaultOptions,
+		position,
 	});
 };
