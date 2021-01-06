8a8de186203185bdabebfbb57d6346a05d3b408f
Jason van den Berg <jayvdb1@gmail.com>
2021-01-06 14:26:45 +0200

    Device language fix

--- src/utils/translations/index.ts
+++ src/utils/translations/index.ts
@@ -4,17 +4,23 @@ import { Platform, NativeModules } from 'react-native';
 import resources from './locales';
 
 const getDeviceLanguage = (): string => {
+	let language = '';
 	if (Platform.OS === 'ios') {
-		return (
+		language =
 			NativeModules.SettingsManager.settings.AppleLocale ||
-			NativeModules.SettingsManager.settings.AppleLanguages[0]
-		);
+			NativeModules.SettingsManager.settings.AppleLanguages[0];
+	} else {
+		language = NativeModules.I18nManager.localeIdentifier;
 	}
 
-	return NativeModules.I18nManager.localeIdentifier;
-};
+	// Android returns specific locales that iOS does not i.e. en_US, en_GB.
+	// If we want to support very specific locales we'll need to add a way to map them here.
+	if (language.indexOf('_')) {
+		language = language.split('_')[0];
+	}
 
-console.warn(getDeviceLanguage());
+	return language;
+};
 
 i18n
 	.use(initReactI18next)
