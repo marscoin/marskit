71e9e966dac1ee3a4032bd06e02a788cf9d64ba8
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-01-09 13:39:08 -0500

    Added all shapes and reset actions to their respective reducers.

--- src/store/reducers/settings.ts
+++ src/store/reducers/settings.ts
@@ -1,29 +1,17 @@
 import actions from '../actions/actions';
 import { ISettings } from '../types/settings';
-import { arrayTypeItems } from '../shapes/wallet';
+import { defaultSettingsShape } from '../shapes/settings';
 
-const settings = (
-	state: ISettings = {
-		loading: false,
-		error: false,
-		biometrics: false,
-		pin: false,
-		pinAttemptsRemaining: 5,
-		theme: 'dark',
-		bitcoinUnit: 'satoshi', //BTC, mBTC, μBTC or satoshi
-		selectedCurrency: 'USD',
-		exchangeRateService: 'bitfinex',
-		selectedLanguage: 'english',
-		customElectrumPeers: arrayTypeItems,
-	},
-	action,
-): ISettings => {
+const settings = (state = defaultSettingsShape, action): ISettings => {
 	switch (action.type) {
 		case actions.UPDATE_SETTINGS:
 			return {
 				...state,
 				...action.payload,
 			};
+
+		case actions.RESET_SETTINGS_STORE:
+			return defaultSettingsShape;
 
 		default:
 			return state;
