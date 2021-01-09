d746e772dd18dfc68952342cec1d9e78b6476a84
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-01-09 13:36:15 -0500

    Added omnibolt, settings & user shapes. Updated defaultWalletStoreShape to shapes/wallet.ts. Updated ICustomElectrumPeer type in types/settings.ts.

---
+++ src/store/shapes/omnibolt.ts
@@ -0,0 +1,10 @@
+import { IOmniBolt } from '../types/omnibolt';
+import { EWallet } from '../types/wallet';
+
+export const defaultOmniBoltShape: IOmniBolt = {
+	loading: false,
+	error: false,
+	selectedNetwork: EWallet.selectedNetwork,
+	selectedWallet: EWallet.defaultWallet,
+	wallets: {},
+};
---
+++ src/store/shapes/user.ts
@@ -0,0 +1,6 @@
+export const defaultUserShape = {
+	loading: false,
+	error: false,
+	isHydrated: false,
+	isOnline: true,
+};
