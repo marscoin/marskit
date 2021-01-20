8efdaa1132a9cdaa36b581e8651600c0aa51f082
Jason van den Berg <jayvdb1@gmail.com>
2021-01-20 17:43:43 +0200

    Travis lint check

--- build-rn-lightning.sh
+++ build-rn-lightning.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+#Used for Travis CI
+
 echo "***Start of temporary hack for building react-native-lightning. TODO: remove this once published to npm.***"
-cd node_modules/react-native-lightning/ && mkdir dist && yarn protobuf && cd ../../
+cd node_modules/react-native-lightning/
+rm -rf dist
+mkdir dist
+yarn protobuf
+cd ../../
 cp node_modules/react-native-lightning/dist/rpc.js node_modules/react-native-lightning/src/lightning
-sed -i -e 's/dist\/index.js/src\/lightning\/index.ts/g' node_modules/react-native-lightning/package.json
+sed -i -e "s/dist\/index.js/src\/lightning\/index.ts/g" node_modules/react-native-lightning/package.json
 echo "***End of temporary hack.***"
