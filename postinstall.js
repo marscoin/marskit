8b91d14d03e63d48c671b74c2ce9485fd2c3491c
Jason van den Berg <jayvdb1@gmail.com>
2021-01-20 21:36:30 +0200

    Moved rn-lightning build to postinstall

--- postinstall.js
+++ postinstall.js
@@ -12,8 +12,25 @@ function postInstallLinWin() {
 	);
 }
 
+//TODO: remove this once react-native-lightning is published to npm
+function buildRnLightning() {
+	exec(
+		'echo "***Start of temporary hack for building react-native-lightning. TODO: remove this once published to npm.***"\n' +
+		'cd node_modules/react-native-lightning/\n' +
+		'rm -rf dist\n' +
+		'mkdir dist\n' +
+		'yarn protobuf\n' +
+		'cd ../../\n' +
+		'cp node_modules/react-native-lightning/dist/rpc.js node_modules/react-native-lightning/src/lightning\n' +
+		'sed -i -e "s/dist\\/index.js/src\\/lightning\\/index.ts/g" node_modules/react-native-lightning/package.json\n' +
+		'sed -i -e "s/\\"types\\": \\".\\/dist\\/index.d.ts\\",//g" node_modules/react-native-lightning/package.json\n' +
+		'echo "***End of temporary hack.***"'
+	)
+}
+
 if (os.type() === 'Darwin') {
 	postInstallMac();
+	buildRnLightning();
 } else {
 	postInstallLinWin();
 }
