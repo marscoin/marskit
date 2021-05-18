12b1a275579c6d9e3b207d65e8e52aa212a7a7a5
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-05-18 19:03:55 -0400

    Added children prop to OutputSummary component. Removed sendAmount & fee props from OutputSummary component. Added FeeSummary component to SendOnChainTransaction/index.tsx.

--- src/screens/Wallets/SendOnChainTransaction/OutputSummary.tsx
+++ src/screens/Wallets/SendOnChainTransaction/OutputSummary.tsx
@@ -1,63 +1,44 @@
 import { IOutput } from '../../../store/types/wallet';
 import React, { ReactElement } from 'react';
 import { Text, View } from '../../../styles/components';
-import Summary from './Summary';
 import { StyleSheet } from 'react-native';
 
 const OutputSummary = ({
 	outputs = [],
 	changeAddress = '',
-	sendAmount = 0,
-	fee = 0,
 	lightning = false,
+	children = <></>,
 }: {
 	outputs: IOutput[];
 	changeAddress: string;
-	sendAmount: number;
-	fee: number;
 	lightning?: boolean;
+	children?: ReactElement;
 }): ReactElement => {
 	return (
-		<>
+		<View color="transparent">
 			{outputs &&
 				outputs.map(({ address, value }, index) => {
 					if (changeAddress !== address) {
 						return (
-							<View
-								key={`${index}${value}`}
-								color="transparent"
-								style={styles.summaryContainer}>
-								<View color="transparent" style={styles.summary}>
-									{!lightning && (
-										<>
-											<Text style={styles.addressText}>Address:</Text>
-											<Text style={styles.addressText}>{address}</Text>
-										</>
-									)}
-
-									<Summary
-										leftText={lightning ? 'Transfer:' : 'Send:'}
-										rightText={`${sendAmount} sats`}
-									/>
-									<Summary leftText={'Fee:'} rightText={`${fee} sats`} />
-								</View>
+							<View key={`${index}${value}`} color="transparent">
+								{!lightning && (
+									<>
+										<Text style={styles.addressText}>Address:</Text>
+										<Text style={styles.addressText}>{address}</Text>
+									</>
+								)}
 							</View>
 						);
 					}
 				})}
-		</>
+			{children}
+		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	addressText: {
 		textAlign: 'center',
-	},
-	summary: {
-		marginVertical: 20,
-	},
-	summaryContainer: {
-		marginVertical: 5,
 	},
 });
 
