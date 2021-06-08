8a60cb3553bc52fac6d06a9defe2faf17997269f
Jason van den Berg <jayvdb1@gmail.com>
2021-06-08 16:54:45 +0200

    Replaced lnurl logic with react-native-lnurl package

--- __tests__/lnurl.ts
+++ __tests__/lnurl.ts
@@ -3,19 +3,14 @@ import {
 	deriveLinkingKeys,
 	getLNURLParams,
 	signK1,
-} from '../src/utils/lnurl';
-import { createWallet } from '../src/store/actions/wallet';
-import { getKeychainValue } from '../src/utils/helpers';
-import { networks } from '../src/utils/networks';
+	LNURLAuthParams,
+} from '@synonymdev/react-native-lnurl';
+import { EAvailableNetworks } from '@synonymdev/react-native-lnurl/dist/utils/types';
+
+const mnemonic =
+	'stable inch effort skull suggest circle charge lemon amazing clean giant quantum party grow visa best rule icon gown disagree win drop smile love';
 
 describe('LN URL', () => {
-	beforeAll(async () => {
-		await createWallet({
-			mnemonic:
-				'stable inch effort skull suggest circle charge lemon amazing clean giant quantum party grow visa best rule icon gown disagree win drop smile love',
-		});
-	});
-
 	it('Decodes LNURL-auth, derives linking keys, signs k1 and created callback URL', async () => {
 		const lnurlAuth =
 			'lnurl1dp68gurn8ghj7ctsdyh8getnw3hx2apwd3hx6ctjddjhguewvdhk6tmvde6hymp0vylhgct884kx7emfdcnxkvfa8yunje3cxqunjdpcxg6nyvenvdjxxcfex56nwvfjxgckxdfhvgunzvtzxesn2ef5xv6rgc348ycnsvpjv43nxcfnxd3kgcfsvymnsdpxdpkkzceav5crzce38yekvcejxumxgvrrxqmkzc3svycnwdp5xgunxc33vvekxwf3vv6nvwf3xqux2vrrvfsnydryxvurgcfsxcmrjdp4v5cr2dgx0xng4';
@@ -27,12 +22,12 @@ describe('LN URL', () => {
 			return;
 		}
 
-		const { data: walletSeed } = await getKeychainValue({ key: 'wallet0' });
+		const params = lnurlRes.value as LNURLAuthParams;
 
 		const keysRes = await deriveLinkingKeys(
-			lnurlRes.value.domain,
-			networks.bitcoinTestnet,
-			walletSeed,
+			params.domain,
+			EAvailableNetworks.bitcoinTestnet,
+			mnemonic,
 		);
 
 		expect(keysRes.isOk()).toEqual(true);
@@ -49,7 +44,7 @@ describe('LN URL', () => {
 			);
 		}
 
-		const signRes = await signK1(lnurlRes.value.k1, keysRes.value.privateKey);
+		const signRes = await signK1(params.k1, keysRes.value.privateKey);
 		expect(signRes.isOk()).toEqual(true);
 		if (signRes.isOk()) {
 			expect(signRes.value).toEqual(
@@ -60,11 +55,12 @@ describe('LN URL', () => {
 			return;
 		}
 
-		const callbackUrlRes = createAuthCallbackUrl(
-			lnurlRes.value.callback,
-			signRes.value,
-			keysRes.value.publicKey,
-		);
+		const callbackUrlRes = await createAuthCallbackUrl({
+			params,
+			network: EAvailableNetworks.bitcoinTestnet,
+			bip32Mnemonic: mnemonic,
+		});
+
 		expect(callbackUrlRes.isOk()).toEqual(true);
 		if (callbackUrlRes.isOk()) {
 			expect(callbackUrlRes.value).toEqual(
