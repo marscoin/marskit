5bdb4cefed3ca4618859d254f3391f399f4334c6
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-07-26 10:28:47 -0400

    Created AddressTypePreference component and implemented it into Settings.

---
+++ src/screens/Settings/AddressTypePreference/index.tsx
@@ -0,0 +1,103 @@
+import React, { memo, ReactElement, useMemo } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import {
+	Feather,
+	Text,
+	TouchableOpacity,
+	View,
+	RadioButtonRN,
+} from '../../../styles/components';
+import { useSelector } from 'react-redux';
+import Store from '../../../store/types';
+import { getSelectedAddressType } from '../../../utils/wallet';
+import { updateSelectedAddressType } from '../../../store/actions/wallet';
+import { RadioButtonItem } from '../../../store/types/settings';
+import { TAddressType } from '../../../store/types/wallet';
+import { capitalize } from '../../../utils/helpers';
+
+const setAddressTypePreference = (preference: TAddressType): void => {
+	updateSelectedAddressType({
+		addressType: preference,
+	});
+};
+
+const AddressTypePreference = ({ navigation }): ReactElement => {
+	const selectedWallet = useSelector(
+		(state: Store) => state.wallet.selectedWallet,
+	);
+	const selectedNetwork = useSelector(
+		(state: Store) => state.wallet.selectedNetwork,
+	);
+
+	const addressTypes = useSelector((state: Store) => state.wallet.addressTypes);
+
+	const radioButtons: RadioButtonItem[] = useMemo(() => {
+		return Object.values(addressTypes).map(({ label, type }) => {
+			return { label: capitalize(label), value: type };
+		});
+	}, [addressTypes]);
+
+	const selectedAddressType = getSelectedAddressType({
+		selectedWallet,
+		selectedNetwork,
+	});
+
+	const initialIndex = useMemo((): number => {
+		let index = -1;
+		try {
+			radioButtons.map((button, i) => {
+				if (selectedAddressType === button.value) {
+					index = i + 1;
+				}
+			});
+			return index || -1;
+		} catch (e) {
+			return index;
+		}
+	}, [selectedAddressType]);
+
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity
+				activeOpacity={0.7}
+				onPress={navigation.goBack}
+				style={styles.row}>
+				<Feather style={{}} name="arrow-left" size={30} />
+				<Text style={styles.backText}>Settings</Text>
+			</TouchableOpacity>
+
+			<ScrollView>
+				<Text style={styles.titleText}>Address-Type Preference</Text>
+				<RadioButtonRN
+					data={radioButtons}
+					selectedBtn={(e): void => {
+						setAddressTypePreference(e.value);
+					}}
+					initial={initialIndex}
+				/>
+			</ScrollView>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingRight: 20,
+		paddingLeft: 20,
+	},
+	row: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingLeft: 10,
+		paddingVertical: 8,
+	},
+	backText: {
+		fontSize: 20,
+	},
+	titleText: {
+		marginTop: 30,
+	},
+});
+
+export default memo(AddressTypePreference);
