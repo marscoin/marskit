92cc9d8e9987c7f12aefd5702a2f99d48cc9e8fb
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-08-13 23:48:59 -0400

    Updated Drawer.Navigator props in DrawerNavigator.tsx.

--- src/navigation/drawer/DrawerNavigator.tsx
+++ src/navigation/drawer/DrawerNavigator.tsx
@@ -1,24 +1,29 @@
 import React, { ReactElement } from 'react';
 import TabNavigator from '../tabs/TabNavigator';
 import Settings from '../../screens/Settings';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+	createDrawerNavigator,
+	DrawerNavigationOptions,
+} from '@react-navigation/drawer';
 import { StyleSheet } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
-const screenOptions = {
+const screenOptions: DrawerNavigationOptions = {
 	headerShown: false,
 	gestureEnabled: true,
+	drawerPosition: 'right',
 };
 
 const DrawerNavigator = (): ReactElement => {
 	return (
 		<Drawer.Navigator
-			screenOptions={screenOptions}
+			screenOptions={{
+				...screenOptions,
+				drawerStyle: styles.drawer,
+			}}
 			initialRouteName="Wallets"
-			drawerContent={(props): ReactElement => <Settings {...props} />}
-			drawerPosition="right"
-			drawerStyle={styles.drawer}>
+			drawerContent={(props): ReactElement => <Settings {...props} />}>
 			<Drawer.Screen name="Wallets" component={TabNavigator} />
 		</Drawer.Navigator>
 	);
