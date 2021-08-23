d21576ea516b30f15bd06641ad6e2a6c374494a6
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-08-22 20:50:42 -0400

    Added UPDATE_ELECTRUM_PEERS to reducers/settings.ts.

--- src/store/reducers/settings.ts
+++ src/store/reducers/settings.ts
@@ -3,11 +3,25 @@ import { ISettings } from '../types/settings';
 import { defaultSettingsShape } from '../shapes/settings';
 
 const settings = (state = defaultSettingsShape, action): ISettings => {
+	let selectedNetwork = state.selectedNetwork;
+	if (action.payload?.selectedNetwork) {
+		selectedNetwork = action.payload.selectedNetwork;
+	}
+
 	switch (action.type) {
 		case actions.UPDATE_SETTINGS:
 			return {
 				...state,
 				...action.payload,
+			};
+
+		case actions.UPDATE_ELECTRUM_PEERS:
+			return {
+				...state,
+				customElectrumPeers: {
+					...state.customElectrumPeers,
+					[selectedNetwork]: action.payload.customElectrumPeers,
+				},
 			};
 
 		case actions.RESET_SETTINGS_STORE:
