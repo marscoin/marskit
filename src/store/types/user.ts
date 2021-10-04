4338f2f06954f4125ff31c9fdb4e269385eca1d0
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-04 16:33:06 -0400

    Added viewController to types/user.ts & shapes/user.ts.

--- src/store/types/user.ts
+++ src/store/types/user.ts
@@ -1,7 +1,25 @@
+export type TViewController = 'send' | 'receive';
+
+export type TUserViewController = {
+	[key in TViewController]: IViewControllerData;
+};
+
+export interface IUserViewController {
+	send: IViewControllerData;
+	receive: IViewControllerData;
+}
+
+export interface IViewControllerData {
+	id: string;
+	isOpen: boolean;
+	snapPoint: number;
+}
+
 export interface IUser {
 	loading: boolean;
 	error: boolean;
 	isHydrated: boolean;
 	isOnline: boolean;
+	viewController: TUserViewController;
 	[key: string]: any;
 }
