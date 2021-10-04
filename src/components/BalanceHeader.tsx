9893d23119ea9d665533ad4562425a741f5781f5
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-04 16:29:56 -0400

    Removed Balance from BalanceHeader.tsx and move it into it's own Balance component.

--- src/components/BalanceHeader.tsx
+++ src/components/BalanceHeader.tsx
@@ -1,54 +1,22 @@
-import React, { memo, ReactElement, useCallback } from 'react';
-import { View, Text01M, Display, Title } from '../styles/components';
+import React, { memo, ReactElement } from 'react';
+import { View, Text01M } from '../styles/components';
 import { useBalance } from '../hooks/wallet';
 import { StyleSheet } from 'react-native';
-import { abbreviateNumber } from '../utils/helpers';
+import Balance from './Balance';
 
 /**
  * Displays the total available balance for the current wallet & network.
  */
 const BalanceHeader = (): ReactElement => {
-	const { fiatWhole, fiatDecimal, fiatDecimalValue, fiatSymbol } = useBalance({
+	const { satoshis } = useBalance({
 		onchain: true,
 		lightning: true,
 		tether: true,
 	});
-
-	const Balance = useCallback((): ReactElement => {
-		if (fiatWhole.length > 12) {
-			const { newValue, abbreviation } = abbreviateNumber(fiatWhole);
-			return (
-				<>
-					<Title style={styles.title} color="gray">
-						{fiatSymbol}
-					</Title>
-					<Display size={'54px'}>{newValue}</Display>
-					<Title style={styles.title} color="gray">
-						{abbreviation}
-					</Title>
-				</>
-			);
-		}
-		return (
-			<>
-				<Title style={styles.title} color="gray">
-					{fiatSymbol}
-				</Title>
-				<Display size={'54px'}>{fiatWhole}</Display>
-				<Title style={styles.title} color="gray">
-					{fiatDecimal}
-					{fiatDecimalValue}
-				</Title>
-			</>
-		);
-	}, [fiatWhole, fiatDecimal, fiatDecimalValue, fiatSymbol]);
-
 	return (
 		<View style={styles.container}>
 			<Text01M color="gray">Total Balance</Text01M>
-			<View style={styles.row}>
-				<Balance />
-			</View>
+			<Balance sats={satoshis} />
 		</View>
 	);
 };
@@ -62,12 +30,5 @@ const styles = StyleSheet.create({
 		justifyContent: 'flex-start',
 		marginVertical: 30,
 		marginHorizontal: 10,
-	},
-	row: {
-		flexDirection: 'row',
-	},
-	title: {
-		top: 7,
-		paddingHorizontal: 5,
 	},
 });
