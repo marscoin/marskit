d7398e86c7d62b1269192e57ed637048c0930079
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-04 17:14:05 -0400

    Created SendBottomSheet component and implemented Send/index.ts into it.

---
+++ src/screens/Wallets/Send/SendBottomSheet.tsx
@@ -0,0 +1,36 @@
+import React, { memo, ReactElement, useCallback } from 'react';
+import {
+	resetOnChainTransaction,
+	setupOnChainTransaction,
+} from '../../../store/actions/wallet';
+import { useSelector } from 'react-redux';
+import Store from '../../../store/types';
+import BottomSheetWrapper from '../../../components/BottomSheetWrapper';
+import Send from './index';
+
+const SendBottomSheet = (): ReactElement => {
+	const selectedWallet = useSelector(
+		(state: Store) => state.wallet.selectedWallet,
+	);
+	const selectedNetwork = useSelector(
+		(state: Store) => state.wallet.selectedNetwork,
+	);
+	const assetType = useSelector(
+		(state: Store) => state.user.viewController.send.id,
+	);
+
+	const onClose = useCallback(() => {
+		resetOnChainTransaction({ selectedWallet, selectedNetwork });
+	}, [selectedNetwork, selectedWallet]);
+
+	return (
+		<BottomSheetWrapper
+			view="send"
+			onOpen={setupOnChainTransaction}
+			onClose={onClose}>
+			<Send asset={assetType} />
+		</BottomSheetWrapper>
+	);
+};
+
+export default memo(SendBottomSheet);
