a66febb43d37762176e6a667d961a69764d2be5a
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-04 16:16:10 -0400

    Wrapped Root in gestureHandlerRootHOC to fix Android issue.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import Root from './Root';
 import { name as appName } from './app.json';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => Root);
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(Root));
