75a1efc85b815e303daca2773abbc141ed95bb40
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-05 14:30:29 -0400

    Updated toggleView method in store/actions/user.ts. Added getAssetNetwork method to utils/wallet/index.ts.

--- src/store/actions/user.ts
+++ src/store/actions/user.ts
@@ -2,6 +2,8 @@ import actions from './actions';
 import { getDispatch } from '../helpers';
 import { ok, Result } from '../../utils/result';
 import { IViewControllerData, TViewController } from '../types/user';
+import { defaultViewController } from '../shapes/user';
+import { getAssetNetwork } from '../../utils/wallet';
 
 const dispatch = getDispatch();
 
@@ -17,6 +19,14 @@ export const toggleView = async (payload: {
 	view: TViewController;
 	data: IViewControllerData;
 }): Promise<Result<string>> => {
+	// Reset viewController state for the provided view.
+	if (!payload.data?.isOpen) {
+		payload.data = { ...defaultViewController };
+	}
+	// Assign the correct assetNetwork based on the provided assetName.
+	if (payload.data?.assetName) {
+		payload.data.assetNetwork = getAssetNetwork(payload.data?.assetName);
+	}
 	await dispatch({
 		type: actions.TOGGLE_VIEW,
 		payload,
