f872e9b773c75d6441259df610aa7e4a413ead66
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-15 10:29:26 -0400

    Added NavigationHeader and updated styles in Receive/index.tsx & Send/index.tsx..

--- src/screens/Wallets/Receive/index.tsx
+++ src/screens/Wallets/Receive/index.tsx
@@ -1,13 +1,17 @@
 import React, { memo, ReactElement, useMemo } from 'react';
-import { View, Text01M } from '../../../styles/components';
+import { View } from '../../../styles/components';
 import { StyleSheet } from 'react-native';
 import { capitalize } from '../../../utils/helpers';
 import { getReceiveAddress } from '../../../utils/wallet';
 import { useSelector } from 'react-redux';
 import Store from '../../../store/types';
 import QR from '../../../components/QR';
+import NavigationHeader from '../../../components/NavigationHeader';
 
 const Receive = ({ asset }: { asset?: string }): ReactElement => {
+	const displayBackButton = useSelector(
+		(state: Store) => state.user.viewController.receiveAssetPicker.isOpen,
+	);
 	const header = useMemo(
 		(): string => (asset ? `Receive ${capitalize(asset)}` : 'Receive'),
 		[asset],
@@ -26,9 +30,9 @@ const Receive = ({ asset }: { asset?: string }): ReactElement => {
 		return ' ';
 	}, [selectedNetwork, selectedWallet]);
 	return (
-		<View style={styles.container}>
-			<Text01M style={styles.headerText}>{header}</Text01M>
-			<View style={styles.content}>
+		<View color={'onSurface'} style={styles.container}>
+			<NavigationHeader title={header} displayBackButton={displayBackButton} />
+			<View color={'onSurface'} style={styles.content}>
 				<QR data={receiveAddress} header={false} />
 			</View>
 		</View>
@@ -42,9 +46,6 @@ const styles = StyleSheet.create({
 	content: {
 		flex: 1,
 		marginTop: 20,
-	},
-	headerText: {
-		alignSelf: 'center',
 	},
 });
 
