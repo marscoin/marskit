5c41b6b943e95deb6f7eb94cc5d62a2960ce534c
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-15 10:31:48 -0400

    Created AssetPickerList, ReceiveAssetPickerList.tsx & SendAssetPickerList.tsx.

---
+++ src/screens/Wallets/Send/SendAssetPickerList.tsx
@@ -0,0 +1,26 @@
+import React, { memo, ReactElement, useCallback } from 'react';
+import AssetPickerList from '../../../components/AssetPickerList';
+import { toggleView } from '../../../store/actions/user';
+import { capitalize } from '../../../utils/helpers';
+import { useNavigation } from '@react-navigation/native';
+
+const SendAssetPickerList = (): ReactElement => {
+	const navigation = useNavigation();
+	const onAssetPress = useCallback((asset) => {
+		toggleView({
+			view: 'sendAssetPicker',
+			data: {
+				isOpen: true,
+				snapPoint: 0,
+				asset,
+				assetName: capitalize(asset),
+			},
+		}).then();
+		// @ts-ignore
+		navigation.navigate('send');
+		//eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	return <AssetPickerList headerTitle="Send" onAssetPress={onAssetPress} />;
+};
+
+export default memo(SendAssetPickerList);
