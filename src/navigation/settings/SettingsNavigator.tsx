1e121c1f63b6e27b17cd5feef73ae6faf3e581eb
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-10-15 10:25:26 -0400

    Added PinPad to SettingsNavigator.tsx.

--- src/navigation/settings/SettingsNavigator.tsx
+++ src/navigation/settings/SettingsNavigator.tsx
@@ -14,6 +14,7 @@ import LightningChannels from '../../screens/Settings/Lightning/LightningChannel
 import LightningChannelDetails from '../../screens/Settings/Lightning/LightningChannelDetails';
 import LightningNodeInfo from '../../screens/Settings/Lightning/LightningNodeInfo';
 import TempSettings from '../../screens/Settings/TempSettings';
+import PinPad from '../../components/PinPad';
 
 const Stack = createNativeStackNavigator();
 
@@ -56,6 +57,7 @@ const SettingsNavigator = (): ReactElement => {
 				<Stack.Screen name="LightningNodeInfo" component={LightningNodeInfo} />
 				<Stack.Screen name="ManageSeedPhrase" component={ManageSeedPhrase} />
 				<Stack.Screen name="LndLogs" component={LndLogs} />
+				<Stack.Screen name="Pin" component={PinPad} />
 			</Stack.Group>
 		</Stack.Navigator>
 	);
