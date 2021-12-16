8573ea5b32c37ef4d075fce138922da1fb8a4913
Corey Phillips <corey.lyle.phillips@protonmail.com>
2021-12-16 10:39:34 -0500

    Restructured styles/components.ts.

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -38,37 +38,37 @@ export const DismissIcon = styled(SvgXml).attrs((props) => ({
 	xml: dismiss(props?.color ? props.theme.colors[props.color] : 'white'),
 	width: props?.width ?? '14px',
 	height: props?.height ?? '16px',
-}))``;
+}))({});
 
 export const CameraIcon = styled(SvgXml).attrs((props) => ({
 	xml: camera(props?.color ? props.theme.colors[props.color] : '#636366'),
 	width: props?.width ?? '20.54px',
 	height: props?.height ?? '20.53px',
-}))``;
+}))({});
 
 export const SettingsIcon = styled(SvgXml).attrs((props) => ({
 	xml: settings(props?.color ? props.theme.colors[props.color] : '#636366'),
 	width: props?.width ?? '21.6px',
 	height: props?.height ?? '19.8px',
-}))``;
+}))({});
 
 export const TransferIcon = styled(SvgXml).attrs((props) => ({
 	xml: transferIcon(props?.color ? props.theme.colors[props.color] : '#636366'),
 	height: props?.height ?? '19.8px',
 	width: props?.width ?? '21.6px',
-}))``;
+}))({});
 
 export const BoostIcon = styled(SvgXml).attrs((props) => ({
 	xml: boost(),
 	width: props?.width ?? '39px',
 	height: props?.height ?? '39px',
-}))``;
+}))({});
 
 export const BitcoinIcon = styled(SvgXml).attrs((props) => ({
 	xml: bitcoinIcon(props?.color ? props.theme.colors[props.color] : '#ED8452'),
 	height: props?.height ?? '19.8px',
 	width: props?.width ?? '21.6px',
-}))``;
+}))({});
 
 export const BitcoinCircleIcon = styled(SvgXml).attrs((props) => ({
 	xml: bitcoinCircleIcon(
@@ -76,7 +76,7 @@ export const BitcoinCircleIcon = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '32px',
 	width: props?.width ?? '32px',
-}))``;
+}))({});
 
 export const TetherCircleIcon = styled(SvgXml).attrs((props) => ({
 	xml: tetherCircleIcon(
@@ -84,7 +84,7 @@ export const TetherCircleIcon = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '32px',
 	width: props?.width ?? '32px',
-}))``;
+}))({});
 
 export const LightningIcon = styled(SvgXml).attrs((props) => ({
 	xml: lightningIcon(
@@ -92,19 +92,19 @@ export const LightningIcon = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '19.8px',
 	width: props?.width ?? '21.6px',
-}))``;
+}))({});
 
 export const SendIcon = styled(SvgXml).attrs((props) => ({
 	xml: sentIcon(props?.color ? props.theme.colors[props.color] : 'white'),
 	height: props?.height ?? '17px',
 	width: props?.width ?? '17px',
-}))``;
+}))({});
 
 export const ReceiveIcon = styled(SvgXml).attrs((props) => ({
 	xml: receivedIcon(props?.color ? props.theme.colors[props.color] : 'white'),
 	height: props?.height ?? '17px',
 	width: props?.width ?? '17px',
-}))``;
+}))({});
 
 export const ChevronRight = styled(SvgXml).attrs((props) => ({
 	xml: chevronRightIcon(
@@ -112,7 +112,7 @@ export const ChevronRight = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '12px',
 	width: props?.width ?? '12px',
-}))``;
+}))({});
 
 export const LeftArrow = styled(SvgXml).attrs((props) => ({
 	xml: leftArrowIcon(
@@ -120,7 +120,7 @@ export const LeftArrow = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '16.04px',
 	width: props?.width ?? '20px',
-}))``;
+}))({});
 
 export const Checkmark = styled(SvgXml).attrs((props) => ({
 	xml: checkmarkIcon(
@@ -128,7 +128,7 @@ export const Checkmark = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '16px',
 	width: props?.width ?? '16px',
-}))``;
+}))({});
 
 export const Copy = styled(SvgXml).attrs((props) => ({
 	xml: copyIcon(
@@ -136,7 +136,7 @@ export const Copy = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '16px',
 	width: props?.width ?? '16px',
-}))``;
+}))({});
 
 export const Logo = styled(SvgXml).attrs((props) => ({
 	xml: logo(
@@ -144,225 +144,220 @@ export const Logo = styled(SvgXml).attrs((props) => ({
 	),
 	height: props?.height ?? '46px',
 	width: props?.width ?? '46px',
-}))``;
+}))({});
 
-export const Display = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '48px')};
-`;
+export const Display = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '48px',
+}));
 
-export const Headline = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '34px')};
-`;
+export const Headline = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '34px',
+}));
 
-export const Title = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '22px')};
-`;
+export const Title = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '22px',
+}));
 
-export const Subtitle = styled.Text`
-	${sanFranciscoWeights.bold};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.bold.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '18px')};
-`;
+export const Subtitle = styled.Text((props) => ({
+	...sanFranciscoWeights.bold,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.bold.fontFamily,
+	fontSize: props.size ? props.size : '18px',
+}));
 
-export const Text01M = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '17px')};
-`;
+export const Text01M = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '17px',
+}));
 
-export const Text01S = styled.Text`
-	${sanFranciscoWeights.regular};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.regular.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '17px')};
-`;
+export const Text01S = styled.Text((props) => ({
+	...sanFranciscoWeights.regular,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.regular.fontFamily,
+	fontSize: props.size ? props.size : '17px',
+}));
 
-export const Text02M = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '15px')};
-`;
+export const Text02M = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '15px',
+}));
 
-export const SubHeadM = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-weight: 500;
-	font-size: ${(props): string => (props.size ? props.size : '14px')};
-`;
+export const SubHeadM = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontWeight: 500,
+	fontSize: props.size ? props.size : '14px',
+}));
 
-export const Text02S = styled.Text`
-	${sanFranciscoWeights.regular};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.regular.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '15px')};
-`;
+export const Text02S = styled.Text((props) => ({
+	...sanFranciscoWeights.regular,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.regular.fontFamily,
+	fontSize: props.size ? props.size : '15px',
+}));
 
-export const Caption13M = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '13px')};
-`;
+export const Caption13M = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '13px',
+}));
 
-export const Caption13S = styled.Text`
-	${sanFranciscoWeights.regular};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : '#636366'};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.regular.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '13px')};
-`;
+export const Caption13S = styled.Text((props) => ({
+	...sanFranciscoWeights.regular,
+	color: props.color ? props.theme.colors[props.color] : '#636366',
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.regular.fontFamily,
+	fontSize: props.size ? props.size : '13px',
+}));
 
-export const Caption13Up = styled.Text`
-	${sanFranciscoWeights.semibold};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : '#636366'};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.semibold.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '13px')};
-`;
+export const Caption13Up = styled.Text((props) => ({
+	...sanFranciscoWeights.semibold,
+	color: props.color ? props.theme.colors[props.color] : '#636366',
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.semibold.fontFamily,
+	fontSize: props.size ? props.size : '13px',
+}));
 
-export const Text13S = styled.Text`
-	${sanFranciscoWeights.regular};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.regular.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '13px')};
-`;
+export const Text13S = styled.Text((props) => ({
+	...sanFranciscoWeights.regular,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.regular.fontFamily,
+	fontSize: props.size ? props.size : '13px',
+}));
 
-export const Text13UP = styled.Text`
-	${sanFranciscoWeights.medium};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: sanFranciscoWeights.medium.fontFamily};
-	font-size: ${(props): string => (props.size ? props.size : '13px')};
-`;
+export const Text13UP = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontSize: props.size ? props.size : '13px',
+}));
 
 export const SafeAreaView = styled(_SafeAreaView)`
 	flex: 1;
 	background-color: ${(props): string => props.theme.colors.background};
 `;
 
-export const Container = styled.View`
-	flex: 1;
-	background-color: ${(props): string => props.theme.colors.background};
-`;
+export const Container = styled.View((props) => ({
+	flex: 1,
+	backgroundColor: props.theme.colors.background,
+}));
 
-export const View = styled.View`
-	background-color: ${(props): string =>
-		props.color
-			? props.theme.colors[props.color]
-			: props.theme.colors.background};
-`;
+export const View = styled.View((props) => ({
+	backgroundColor: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.background,
+}));
 
-export const AnimatedView = styled(Animated.View)`
-	background-color: ${(props): string =>
-		props.color
-			? props.theme.colors[props.color]
-			: props.theme.colors.background};
-`;
+export const AnimatedView = styled(Animated.View)((props) => ({
+	backgroundColor: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.background,
+}));
 
-export const TouchableOpacity = styled.TouchableOpacity`
-	background-color: ${(props): string =>
-		props.color
-			? props.theme.colors[props.color]
-			: props.theme.colors.background};
-`;
+export const TouchableOpacity = styled.TouchableOpacity((props) => ({
+	backgroundColor: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.background,
+}));
 
-export const Pressable = styled.Pressable`
-	background-color: ${(props): string =>
-		props.color
-			? props.theme.colors[props.color]
-			: props.theme.colors.background};
-	opacity: ${(props): number => (props.disabled ? 0.4 : 1)};
-`;
+export const Pressable = styled.Pressable((props) => ({
+	backgroundColor: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.background,
+	opacity: props.disabled ? 0.4 : 1,
+}));
 
-export const ScrollView = styled.ScrollView`
-	background-color: ${(props): string => props.theme.colors.background};
-`;
+export const ScrollView = styled.ScrollView((props) => ({
+	backgroundColor: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.background,
+}));
 
 export const TextInput = styled.TextInput.attrs((props) => ({
 	selectionColor: colors.orange,
 	placeholderTextColor: props?.placeholderTextColor
 		? props.placeholderTextColor
 		: 'gray',
-}))`
-	background-color: ${(props): string =>
-		props.backgroundColor
-			? props.theme.colors[props.color]
-			: props.theme.colors.onSurface};
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	border-color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-`;
+}))((props) => ({
+	backgroundColor: props.backgroundColor
+		? props.theme.colors[props.color]
+		: props.theme.colors.onSurface,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	borderColor: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+}));
 
 export const RefreshControl = styled.RefreshControl.attrs((props) => ({
 	tintColor: props.theme.colors.refreshControl,
-}))``;
+}))({});
 
 export const StatusBar = styled.StatusBar.attrs((props) => ({
 	animated: true,
@@ -372,20 +367,20 @@ export const StatusBar = styled.StatusBar.attrs((props) => ({
 			: props.theme.id === 'light'
 			? 'dark-content'
 			: 'light-content',
-}))``;
+}))({});
 
-export const Text = styled.Text`
-	color: ${(props): string =>
-		props.color ? props.theme.colors[props.color] : props.theme.colors.text};
-	font-family: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontFamily
-			: props.theme.fonts.medium.fontFamily};
-	font-weight: ${(props): string =>
-		props.font
-			? props.theme.fonts[props.font].fontWeight
-			: props.theme.fonts.medium.fontWeight};
-`;
+export const Text = styled.Text((props) => ({
+	...sanFranciscoWeights.medium,
+	color: props.color
+		? props.theme.colors[props.color]
+		: props.theme.colors.text,
+	fontFamily: props.font
+		? props.theme.fonts[props.font].fontFamily
+		: sanFranciscoWeights.medium.fontFamily,
+	fontWeight: props.font
+		? props.theme.fonts[props.font].fontWeight
+		: sanFranciscoWeights.medium.fontWeight,
+}));
 
 export const RadioButtonRN = styled(_RadioButtonRN).attrs((props) => ({
 	box: props?.box ? props.box : false,
@@ -395,37 +390,37 @@ export const RadioButtonRN = styled(_RadioButtonRN).attrs((props) => ({
 	activeColor: props?.activeColor
 		? props.activeColor
 		: props.theme.colors.onBackground,
-}))``;
+}))({});
 
 export const Feather = styled(_Feather).attrs((props) => ({
 	color: props.color
 		? props.theme.colors[props.color]
 		: props.theme.colors.text,
-}))``;
+}))({});
 
 export const MaterialIcons = styled(_MaterialIcons).attrs((props) => ({
 	color: props.color
 		? props.theme.colors[props.color]
 		: props.theme.colors.text,
-}))``;
+}))({});
 
 export const AntDesign = styled(_AntDesign).attrs((props) => ({
 	color: props.color
 		? props.theme.colors[props.color]
 		: props.theme.colors.text,
-}))``;
+}))({});
 
 export const EvilIcon = styled(_EvilIcon).attrs((props) => ({
 	color: props.color
 		? props.theme.colors[props.color]
 		: props.theme.colors.text,
-}))``;
+}))({});
 
 export const Ionicons = styled(_Ionicons).attrs((props) => ({
 	color: props.color
 		? props.theme.colors[props.color]
 		: props.theme.colors.text,
-}))``;
+}))({});
 
 export const SafeAreaProvider = styled(_SafeAreaProvider)`
 	flex: 1;
@@ -447,4 +442,4 @@ export const NavigationContainer = styled(_NavigationContainer).attrs(
 			},
 		},
 	}),
-)``;
+)({});
