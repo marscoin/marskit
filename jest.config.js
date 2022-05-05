cb1c2427d6061ee0ade5aeee0153e9624f728733
Jason van den Berg <jayvdb1@gmail.com>
2022-05-05 16:57:27 +0200

    NativeEventEmitter unit test mock

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
 	preset: 'react-native',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	transformIgnorePatterns: ['<roodDir>/__mocks__'],
+	setupFiles: ['<rootDir>/jest.setup.js'],
 };
