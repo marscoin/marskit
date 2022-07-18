2f20281f67b3cf620548f97f531d44763515cd03
nazeh <ar.nazeh@gmail.com>
2022-07-18 19:20:50 +0300

    feat(hooks): update for useSlashtagProfile hook

--- src/hooks/slashtags.ts
+++ src/hooks/slashtags.ts
@@ -1,46 +1,53 @@
 import type { SDK as ISDK } from '@synonymdev/slashtags-sdk/types/src/index';
+import type { EventsListeners } from '@synonymdev/slashdrive';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import { useSlashtags } from '../components/SlashtagsProvider';
-import Store from '../store/types';
 import { BasicProfile } from '../store/types/slashtags';
 
 export type Slashtag = ReturnType<ISDK['slashtag']>;
 
 /**
- * Helper hook to return Slashtag's profile
- * and detect when it has a new version to fetch.
+ * Helper hook to get and set profiles.
  */
-export const useSlashtagProfile = (opts?: { url: string }): BasicProfile => {
+export const useSlashtagProfile = (opts?: {
+	url: string;
+}): [BasicProfile, (p: BasicProfile) => void] => {
 	const [profile, setProfile] = useState<BasicProfile>({});
 
 	const { sdk } = useSlashtags();
-
 	const slashtag = sdk?.slashtag(opts);
-	const url = slashtag?.url.toString();
 
-	const version = useSelector(
-		(store: Store) => store.slashtags.profiles?.[url || '']?.seen,
-	);
-
-	useEffect((): (() => void) => {
-		// set a clean up flag
-		let shouldSetProfile = true;
-
-		const _slashtag = sdk?.slashtag({ url });
-
-		_slashtag?.getProfile().then((p) => {
-			shouldSetProfile &&
-				setProfile({
-					...(p || {}),
-					id: _slashtag.url.toString(),
-				});
+	useEffect(() => {
+		slashtag?.getProfile().then((p) => {
+			setProfile({
+				...(p || {}),
+				id: slashtag.url.toString(),
+			});
 		});
 
-		return () => {
-			shouldSetProfile = false;
+		const listener: EventsListeners['update'] = async ({ key }) => {
+			if (key === 'profile.json') {
+				slashtag?.getProfile().then((p) => {
+					setProfile({
+						...(p || {}),
+						id: slashtag.url.toString(),
+					});
+				});
+			}
 		};
-	}, [sdk, url, version]);
 
-	return profile;
+		slashtag?.publicDrive?.on('update', listener);
+
+		return (): void => {
+			slashtag?.publicDrive?.removeListener('update', listener);
+		};
+	}, [slashtag]);
+
+	return [
+		profile,
+		(toSave): void => {
+			setProfile(toSave);
+			slashtag?.setProfile(toSave);
+		},
+	];
 };
