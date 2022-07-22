03648a3c15689ac1247d81365ed8460a037f6514
Ivan Vershigora <ivan.vershigora@gmail.com>
2022-07-22 12:50:30 +0300

    Fix jest mocks config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	preset: 'react-native',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	transformIgnorePatterns: ['<roodDir>/__mocks__'],
-	setupFiles: ['<rootDir>/jest.setup.js'],
+	setupFiles: [
+		'<rootDir>/jest.setup.js',
+		'./node_modules/react-native-gesture-handler/jestSetup.js',
+	],
 };
