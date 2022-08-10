9633d91dcca0b768f6bd8d78e2c95087891ab45d
Ivan Vershigora <ivan.vershigora@gmail.com>
2022-08-10 09:52:19 +0300

    feat: splash screen

--- android/app/src/main/java/com/bitkit/MainApplication.java
+++ android/app/src/main/java/com/bitkit/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.bitkit.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.bitkit.modules.SplashScreen.SplashScreenPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new SplashScreenPackage());
           return packages;
         }
 
