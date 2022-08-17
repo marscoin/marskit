0f8fa9b9b915e23a7d8a5f623d2bd7c9d6d9435e
Philipp Walter <philippwalter@proton.me>
2022-08-17 15:26:37 +0200

    fix: deprecated redux store config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,43 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { persistReducer } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
 import * as Sentry from '@sentry/react-native';
+import logger from 'redux-logger';
+import {
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
 
 import mmkvStorage from './mmkv-storage';
 import reducers from './reducers';
 
-const sentryReduxEnhancer = Sentry.createReduxEnhancer({});
+const __JEST__ = process.env.JEST_WORKER_ID !== undefined;
 
-//Switch off logging for unit tests and prod env
-const createStoreWithMiddleware = compose(
-	process.env.JEST_WORKER_ID === undefined && __DEV__
-		? applyMiddleware(thunk, logger)
-		: applyMiddleware(thunk),
-	sentryReduxEnhancer,
-)(createStore);
+// Switch off logging for unit tests and prod env
+const devMiddleware = __DEV__ && !__JEST__ ? [logger] : [];
+const sentryReduxEnhancer = Sentry.createReduxEnhancer();
 
-const persistConfig = {
-	key: 'root',
-	storage: mmkvStorage,
-};
+const persistConfig = { key: 'root', storage: mmkvStorage };
 const persistedReducer = persistReducer(persistConfig, reducers);
-const store = createStoreWithMiddleware(persistedReducer);
+
+const store = configureStore({
+	reducer: persistedReducer,
+	enhancers: [sentryReduxEnhancer],
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			// specifically ignore redux-persist action types
+			// https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat(devMiddleware),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
