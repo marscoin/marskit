297d2761ce341b9cea6f87e194bf815cd54ce992
Philipp Walter <philippwalter@proton.me>
2022-09-07 20:29:10 +0200

    fix(settings): freeze when resetting suggestions

--- src/screens/Settings/General/index.tsx
+++ src/screens/Settings/General/index.tsx
@@ -1,18 +1,31 @@
-import React, { memo, ReactElement, useEffect, useMemo, useState } from 'react';
+import React, { memo, ReactElement, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Modal, StyleSheet, Text, Pressable, View } from 'react-native';
+
 import Store from './../../../store/types';
 import { IListData } from './../../../components/List';
 import SettingsView from './../SettingsView';
-import { getStore } from '../../../store/helpers';
 import { updateSettings } from '../../../store/actions/settings';
 import { setupTodos } from '../../../utils/todos';
 import { resetTodo } from '../../../store/actions/todos';
+import colors from '../../../styles/colors';
 
 const typesDescriptions = {
 	p2wpkh: 'Bech32',
 	p2sh: 'Segwit',
 	p2pkh: 'Legacy',
+};
+
+const unitsBitcoin = {
+	satoshi: 'Satoshis',
+	BTC: 'Bitcoin',
+};
+
+const transactionSpeeds = {
+	slow: 'Slow',
+	normal: 'Normal',
+	fast: 'Fast',
+	custom: 'Custom',
 };
 
 const General = ({ navigation }): ReactElement => {
@@ -26,16 +39,6 @@ const General = ({ navigation }): ReactElement => {
 		(store: Store) => store.settings.addressType,
 	);
 
-	// set a default value to transaction speed if it's not set
-	useEffect(() => {
-		const transactionSpeed = getStore().settings.transactionSpeed;
-		if (!transactionSpeed) {
-			updateSettings({
-				transactionSpeed: 'normal',
-			});
-		}
-	}, []);
-
 	const selectedTransactionSpeed = useSelector(
 		(state: Store) => state.settings.transactionSpeed,
 	);
@@ -47,18 +50,6 @@ const General = ({ navigation }): ReactElement => {
 	const selectedBitcoinUnit = useSelector(
 		(state: Store) => state.settings.bitcoinUnit,
 	);
-
-	const unitsBitcoin = {
-		satoshi: 'Satoshis',
-		BTC: 'Bitcoin',
-	};
-
-	const transactionSpeeds = {
-		slow: 'Slow',
-		normal: 'Normal',
-		fast: 'Fast',
-		custom: 'Custom',
-	};
 
 	const SettingsListData: IListData[] = useMemo(
 		() => [
@@ -137,25 +128,37 @@ const General = ({ navigation }): ReactElement => {
 				transparent={true}
 				visible={modalVisible}
 				onRequestClose={(): void => {
-					setModalVisible(!modalVisible);
+					setModalVisible(false);
 				}}>
 				<View style={styles.centeredView}>
 					<View style={styles.modalView}>
-						<Text style={styles.modalTitle}>Reset Suggestions?</Text>
-						<Text style={styles.modalText}>
-							Are you sure you want to reset the suggestions? They will reappear
-							in case you have removed them from your Bitkit wallet overview.
-						</Text>
-						<Pressable
-							style={[styles.button]}
-							onPress={(): void => {
-								resetTodo();
-								setupTodos();
-								setModalVisible(!modalVisible);
-							}}>
-							<Text style={styles.buttonText}>No, Cancel</Text>
-							<Text style={styles.buttonText}>Yes, Reset</Text>
-						</Pressable>
+						<View style={styles.modalText}>
+							<Text style={styles.modalTitle}>Reset Suggestions?</Text>
+							<Text style={styles.modalDescription}>
+								Are you sure you want to reset the suggestions? They will
+								reappear in case you have removed them from your Bitkit wallet
+								overview.
+							</Text>
+						</View>
+						<View style={styles.buttons}>
+							<Pressable
+								style={[styles.button, styles.buttonLeft]}
+								onPress={(): void => {
+									setModalVisible(false);
+								}}>
+								<Text style={styles.buttonText}>No, Cancel</Text>
+							</Pressable>
+							<Pressable
+								style={styles.button}
+								onPress={(): void => {
+									resetTodo();
+									// add timeout to avoid a bug
+									setTimeout(() => setupTodos(), 2000);
+									setModalVisible(false);
+								}}>
+								<Text style={styles.buttonText}>Yes, Reset</Text>
+							</Pressable>
+						</View>
 					</View>
 				</View>
 			</Modal>
@@ -176,10 +179,9 @@ const styles = StyleSheet.create({
 		backgroundColor: 'rgba(49, 49, 49, 1)',
 		backdropFilter: 'blur(27.1828px)',
 		borderRadius: 14,
-		padding: 16,
 		alignItems: 'center',
-		shadowColor: '#000',
-		color: '#fff',
+		shadowColor: colors.black,
+		color: colors.white,
 		shadowOffset: {
 			width: 0,
 			height: 2,
@@ -188,6 +190,9 @@ const styles = StyleSheet.create({
 		shadowRadius: 4,
 		elevation: 5,
 	},
+	modalText: {
+		padding: 16,
+	},
 	modalTitle: {
 		fontWeight: '600',
 		fontSize: 17,
@@ -195,13 +200,29 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		letterSpacing: -0.41,
 		textTransform: 'capitalize',
-		color: '#FFFFFF',
+		color: colors.white,
 		marginBottom: 5,
 	},
-	button: {
-		display: 'flex',
-		justifyContent: 'space-between',
+	modalDescription: {
+		fontWeight: '500',
+		fontSize: 13,
+		lineHeight: 18,
+		letterSpacing: -0.08,
+		textAlign: 'center',
+		color: colors.white,
+	},
+	buttons: {
 		flexDirection: 'row',
+	},
+	button: {
+		borderTopWidth: 1,
+		borderColor: colors.gray3,
+		padding: 16,
+		flex: 1,
+	},
+	buttonLeft: {
+		borderRightWidth: 1,
+		borderColor: colors.gray3,
 	},
 	buttonText: {
 		fontWeight: '400',
@@ -210,19 +231,7 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		letterSpacing: -0.41,
 		textTransform: 'capitalize',
-		color: '#FF6600',
-		width: 119,
-	},
-	modalText: {
-		fontWeight: '500',
-		fontSize: 13,
-		lineHeight: 18,
-		letterSpacing: -0.08,
-		textAlign: 'center',
-		color: '#fff',
-		paddingLeft: 16,
-		paddingRight: 16,
-		marginBottom: 16,
+		color: colors.brand,
 	},
 });
 
