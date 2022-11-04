53bf9aaef04a76ed7c89a3f8dee4de45e5fb9822
Corey <coreyphillips@users.noreply.github.com>
2022-11-04 07:16:06 -0400

    Update src/navigation/lightning/LightningNavigator.tsx
    Co-authored-by: Philipp Walter <philippwalter@proton.me>

--- src/navigation/lightning/LightningNavigator.tsx
+++ src/navigation/lightning/LightningNavigator.tsx
@@ -32,7 +32,7 @@ export type LightningStackParamList = {
 	Result: undefined;
 	QuickSetup: {
 		headerTitle: string;
-	};
+	} | undefined;
 	QuickConfirm: {
 		spendingAmount: number;
 		total: number;
