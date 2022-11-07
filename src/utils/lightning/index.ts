6f6c02a60f371b285c7644c00e9ec5b04b801ec2
Corey Phillips <corey.lyle.phillips@protonmail.com>
2022-11-07 10:29:22 -0500

    fix(lightning): Update react-native-ldk
    Updates react-native-ldk.
    Removes iOS conditions for channel open.
    Use default LDK feeRate.

--- src/utils/lightning/index.ts
+++ src/utils/lightning/index.ts
@@ -44,11 +44,7 @@ import RNFS from 'react-native-fs';
 import { EmitterSubscription } from 'react-native';
 import { EActivityTypes, IActivityItem } from '../../store/types/activity';
 import { addActivityItem } from '../../store/actions/activity';
-import {
-	EPaymentType,
-	ETransactionDefaults,
-	IWalletItem,
-} from '../../store/types/wallet';
+import { EPaymentType, IWalletItem } from '../../store/types/wallet';
 import { toggleView } from '../../store/actions/user';
 import { updateSlashPayConfig } from '../slashtags';
 import { sdk } from '../../components/SlashtagsProvider';
@@ -182,7 +178,6 @@ export const setupLdk = async ({
 			broadcastTransaction: _broadcastTransaction,
 			getTransactionData,
 			network,
-			feeRate: ETransactionDefaults.recommendedBaseFee,
 		});
 
 		if (lmStart.isErr()) {
