261afe30d8f7a6ea91308b1247fb6faa2b822dda
Corey Phillips <corey.lyle.phillips@protonmail.com>
2022-12-07 11:56:57 -0500

    fix(wallet): Revert PieChart
    Reverted PieChart changes.

--- src/screens/Lightning/PieChart.tsx
+++ src/screens/Lightning/PieChart.tsx
@@ -1,4 +1,4 @@
-import React, { memo, ReactElement, useMemo } from 'react';
+import React, { memo, ReactElement } from 'react';
 import {
 	DashPathEffect,
 	BlurMask,
@@ -23,122 +23,111 @@ const PieChart = ({
 	dashed?: number;
 }): ReactElement => {
 	const colors = useColors();
-	const whole = useMemo(() => size + shift * 2, [shift, size]);
-	const r = useMemo(() => size / 2, [size]);
-	const center = useMemo(() => shift + r, [r, shift]);
-	const cx = useMemo(() => center, [center]);
-	const cy = useMemo(() => center, [center]);
+	const whole = size + shift * 2;
+	const r = size / 2;
+	const center = shift + r;
+	const cx = center;
+	const cy = center;
 
-	const content = useMemo(() => {
-		if (primary === 0 || primary === 100) {
-			// full circle
-			const color = primary === 100 ? colors.purple : colors.orange;
-			return (
-				<Path
-					color={color}
-					style="stroke"
-					strokeJoin="round"
-					strokeWidth={1.5}
-					path={`
+	let content;
+	if (primary === 0 || primary === 100) {
+		// full circle
+		const color = primary === 100 ? colors.purple : colors.orange;
+		content = (
+			<Path
+				color={color}
+				style="stroke"
+				strokeJoin="round"
+				strokeWidth={1.5}
+				path={`
 				    M ${cx} ${cy}
 				    m -${r}, 0
 				    a ${r},${r} 0 1,1 ${r * 2},0
 				    a ${r},${r} 0 1,1 -${r * 2},0
 			    `}>
-					{primary === 100 && <Paint color={colors.purple16} />}
-				</Path>
-			);
-		} else {
-			// primary angle in radians
-			const pandgle = (2 * Math.PI * primary) / 100;
-			// relative coordinations of breaking point
-			const x = r * Math.sin(pandgle);
-			const y = r * Math.cos(pandgle);
-			// abosolute coordinates of breaking point
-			const px = center + x;
-			const py = center - y;
-			// which path to choose
-			const primaryLargeArcFlag = x < 0 ? 1 : 0;
-			const secondaryLargeArcFlag = !primaryLargeArcFlag ? 1 : 0;
+				{primary === 100 && <Paint color={colors.purple16} />}
+			</Path>
+		);
+	} else {
+		// primary angle in radians
+		const pandgle = (2 * Math.PI * primary) / 100;
+		// relative coordinations of breaking point
+		const x = r * Math.sin(pandgle);
+		const y = r * Math.cos(pandgle);
+		// abosolute coordinates of breaking point
+		const px = center + x;
+		const py = center - y;
+		// which path to choose
+		const primaryLargeArcFlag = x < 0 ? 1 : 0;
+		const secondaryLargeArcFlag = !primaryLargeArcFlag ? 1 : 0;
 
-			return (
-				<>
-					<Path
-						color={colors.orange}
-						style="stroke"
-						strokeJoin="round"
-						strokeWidth={1.5}
-						path={`
+		content = (
+			<>
+				<Path
+					color={colors.orange}
+					style="stroke"
+					strokeJoin="round"
+					strokeWidth={1.5}
+					path={`
 				    M ${px} ${py}
 				    A ${r},${r} 0 ${secondaryLargeArcFlag},1 ${cx},${cx - r}
 			    `}
-					/>
-					<Path
-						color={colors.purple}
-						style="stroke"
-						strokeJoin="round"
-						strokeWidth={1.5}
-						path={`
+				/>
+				<Path
+					color={colors.purple}
+					style="stroke"
+					strokeJoin="round"
+					strokeWidth={1.5}
+					path={`
 				    M ${cx} ${cy}
 				    m 0, -${r}
 				    A ${r},${r} 0 ${primaryLargeArcFlag},1 ${px},${py}
 				    L ${cx} ${cy}
 				    l 0, -${r}
 			    `}>
-						<Paint color={colors.purple16} />
-					</Path>
-				</>
-			);
-		}
-	}, [
-		center,
-		colors.orange,
-		colors.purple,
-		colors.purple16,
-		cx,
-		cy,
-		primary,
-		r,
-	]);
+					<Paint color={colors.purple16} />
+				</Path>
+			</>
+		);
+	}
 
-	const dashedLine = useMemo(() => {
-		if (dashed !== undefined) {
-			// primary angle in radians
-			const pandgle = (2 * Math.PI * dashed) / 100;
-			// relative coordinations of breaking point
-			const x = r * Math.sin(pandgle);
-			const y = r * Math.cos(pandgle);
-			// abosolute coordinates of breaking point
-			const px = center + x;
-			const py = center - y;
-			return (
-				<>
-					<Path
-						color={colors.black}
-						style="stroke"
-						strokeJoin="round"
-						strokeWidth={2}
-						path={`
+	let dashedLine;
+	if (dashed !== undefined) {
+		// primary angle in radians
+		const pandgle = (2 * Math.PI * dashed) / 100;
+		// relative coordinations of breaking point
+		const x = r * Math.sin(pandgle);
+		const y = r * Math.cos(pandgle);
+		// abosolute coordinates of breaking point
+		const px = center + x;
+		const py = center - y;
+		dashedLine = (
+			<>
+				<Path
+					color={colors.black}
+					style="stroke"
+					strokeJoin="round"
+					strokeWidth={2}
+					path={`
 				    M ${cx} ${cy}
 				    L ${px} ${py}
 			    `}>
-						<Paint color={colors.black} />
-					</Path>
-					<Path
-						color={colors.purple}
-						style="stroke"
-						strokeJoin="round"
-						strokeWidth={2}
-						path={`
+					<Paint color={colors.black} />
+				</Path>
+				<Path
+					color={colors.purple}
+					style="stroke"
+					strokeJoin="round"
+					strokeWidth={2}
+					path={`
 				    M ${cx} ${cy}
 				    L ${px} ${py}
 			    `}>
-						<DashPathEffect intervals={[2, 2]} />
-					</Path>
-				</>
-			);
-		}
-	}, [center, colors.black, colors.purple, cx, cy, dashed, r]);
+					<DashPathEffect intervals={[2, 2]} />
+				</Path>
+			</>
+		);
+	}
 
 	return (
 		<Canvas style={{ width: whole, height: whole }}>
